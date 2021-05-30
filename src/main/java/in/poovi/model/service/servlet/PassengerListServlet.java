5984597ce51f9b2cf1b09eb6df29230f0407f1d2
poovitha22 <82588990+Poovitha22@users.noreply.github.com>
2021-05-30 18:19:11 +0530

    Update PassengerListServlet.java
    code smell fix

--- src/main/java/in/poovi/model/service/servlet/PassengerListServlet.java
+++ src/main/java/in/poovi/model/service/servlet/PassengerListServlet.java
@@ -1,9 +1,6 @@
 package in.poovi.model.service.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,30 +10,19 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 
 import in.poovi.dao.PassengerDAO;
-//import in.poovi.dao.PassengerDaoImpl;
-//import in.poovi.exception.DBException;
 import in.poovi.model.PassengerModel;
 import in.poovi.model.service.PassengerService;
 
-/**
- * Servlet implementation class PassengerListServlet
- */
+
 @WebServlet("/PassengerListServlet")
 public class PassengerListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public PassengerListServlet() {
 		super();
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
          try {
         	 int pid=Integer.parseInt(request.getParameter("pid"));
