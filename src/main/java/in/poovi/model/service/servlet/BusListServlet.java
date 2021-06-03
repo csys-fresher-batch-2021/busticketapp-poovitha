dc63dd6a5ac38e41d20de2d55c36add649c74337
poovitha22 <82588990+Poovitha22@users.noreply.github.com>
2021-06-03 16:18:29 +0530

    Update BusListServlet.java
    code smell fix

--- src/main/java/in/poovi/model/service/servlet/BusListServlet.java
+++ src/main/java/in/poovi/model/service/servlet/BusListServlet.java
@@ -1,9 +1,9 @@
  package in.poovi.model.service.servlet;
 
 import java.io.IOException;
-//import java.sql.Time;
+
 import java.time.LocalTime;
-//import java.util.List;
+
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -15,24 +15,14 @@ import javax.servlet.http.HttpServletResponse;
 import in.poovi.model.BusDetails;
 import in.poovi.model.service.BusDetailsService;
 
-/**
- * Servlet implementation class BusListServlet
- */
 @WebServlet("/BusListServlet")
 public class BusListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public BusListServlet() {
 		super();
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
@@ -43,10 +33,6 @@ public class BusListServlet extends HttpServlet {
 			int busno = Integer.parseInt(request.getParameter("b_no"));
 			String bustype = request.getParameter("bustype");
 			double amount = Integer.parseInt(request.getParameter("amount"));
-			// LocalTime
-			//List<BusDetails> busdetails = BusDetailsService.getBusDetails();
-            // deparaturetime=LocalTime.parse(request.getParameter("departuretime"));
-			// LocalTime arrivaltime=LocalTime.parse(request.getParameter("arrivaltime"));
 			try {
 	     	BusDetails bus = new BusDetails(agency, busno, bustype, amount);
 			BusDetailsService busdetailsservice = new BusDetailsService();
@@ -55,7 +41,6 @@ public class BusListServlet extends HttpServlet {
 			String message = "added";
 			response.sendRedirect("BusList.jsp?infomessage=" + message);
 
-			// response.sendRedirect("addBusList.jsp");
 
 		} catch (Exception e) {
 			e.printStackTrace();
