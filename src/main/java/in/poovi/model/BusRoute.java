b846ff54936bcc13e4ca8d1beb959fd6c1cfe14a
poovitha22 <82588990+Poovitha22@users.noreply.github.com>
2021-06-09 13:21:06 +0530

    Update BusRoute.java
    error fix

--- src/main/java/in/poovi/model/BusRoute.java
+++ src/main/java/in/poovi/model/BusRoute.java
@@ -40,6 +40,7 @@ public class BusRoute {
 
 	public void setDestination(String destination) {
 		this.destination = destination;
+	}
 	@Override
 	public String toString() {
 		return "BusRoute [routeNo=" + routeNo + ", source=" + source + ", destination=" + destination + "]";
