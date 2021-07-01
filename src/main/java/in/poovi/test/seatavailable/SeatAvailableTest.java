bc130c10d059185c5195367b9e21467d51734551
Poovitha T <poovithayamini228@gmail.com>
2021-07-01 11:09:31 +0530

    fix code smell

--- src/main/java/in/poovi/test/seatavailable/SeatAvailableTest.java
+++ src/main/java/in/poovi/test/seatavailable/SeatAvailableTest.java
@@ -2,6 +2,7 @@ package in.poovi.test.seatavailable;
 
 import in.poovi.dao.SeatAvailableDAO;
 import in.poovi.dao.impl.SeatAvailableDAOImpl;
+import in.poovi.exception.DBException;
 import in.poovi.model.SeatAvailable;
 
 public class SeatAvailableTest {
@@ -10,9 +11,9 @@ public class SeatAvailableTest {
 	 * This main method is used to get the seat available by busnumber
 	 * 
 	 * @param args
-	 * @throws Exception
+	 * @throws DBException
 	 */
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws DBException {
 
 		SeatAvailableDAO seatavailableDAO = new SeatAvailableDAOImpl();
 		SeatAvailable seat = new SeatAvailable();
